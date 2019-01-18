5b877114e01414c8ea82cbeeb7ab5d28dcd9054f
Isau Opeyemi AZEEZ <prodigy4442@gmail.com>
2019-01-18 04:16:40 +0100

    Changing test type

--- src/main/java/ng/digitalpulse/smpp/module/SmppModule.java
+++ src/main/java/ng/digitalpulse/smpp/module/SmppModule.java
@@ -20,7 +20,7 @@ public class SmppModule {
 //                SmppBindType.TRANSMITTER, "172.24.11.125", 31110);
         
         Session session = new Session("Airtel", "JAMB", "JAMB", "z6+Wc$4v", null, 
-                SmppBindType.TRANSMITTER, "172.24.11.125", 31110, 2);
+                SmppBindType.TRANSCEIVER, "172.24.11.125", 31110, 2);
         
         TimeUnit.SECONDS.sleep(10);
         session.bindSession();
